c857a67fc92176d2facb9f0bd6489ae58512d1b5
jafar <mjmohammadi1@gmail.com>
2021-07-14 19:34:50 -0400

    fix: clean up unused code

--- src/utils/queue.js
+++ src/utils/queue.js
@@ -3,11 +3,9 @@ const CONCLIMIT = 1;
 const queue = require('fastq').promise(worker, CONCLIMIT);
 
 async function worker({ work, params }) {
-  // console.log('queue called')
   try {
     return work(params);
   } catch (err) {
-    // console.log('err in queue')
     throw new Error(err);
   }
 }
